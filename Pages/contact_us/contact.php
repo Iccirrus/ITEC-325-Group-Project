65b05f06c6993703601272cefba40517feec000e
lingenedil <lingenedil@gmail.com>
2015-10-24 19:32:47 -0400

    Various updates
    Form page, staff page, client side form validation

--- Pages/contact_us/contact.php
+++ Pages/contact_us/contact.php
@@ -1,35 +1,20 @@
 <?php
 
-   $name = $_POST[contactName];
-   $email = $_POST[contactEmail];
-   $messageTo = $_POST[recipient];
-   $SubjectOf = $_POST[subject];
-   $sentMessage = $_POST[messageOut];
+if(!empty($_POST) {
+   $name = $_POST['contactName'];
+   $email = $_POST['contactEmail'];
+   $subject = $_POST['subject'];
+   $sentMessage = $_POST['messageOut'];
 
-   $mailTo = $messageTo;
-   $subject = $subjectOf;
+   $body = 'From: '.$name."\n".'E-Mail: '.$email."\n".'Message: '.$sentMessage;
 
-   $body = 'From: '.$name."\n";
-   $body = 'E-Mail: '.$email."\n";
-   $body = 'Message: '.$sentMessage;
+   $head = 'From: '.$email."\r\n".'Reply-To: '.$email."\r\n";
 
-   $head = 'From: '.$email."\r\n";
-   $head = 'Reply-To: '.$email."\r\n";
-
-   $status = mail(mailTo, $subject, $body, $head);
-
-   if ($status) ( ?>
-	<script language = "javascript" type = "text/javascript">
-	   alert('Thank you for the message.  We will contact you shortly.');
-	   window.location = 'contactForm.html';
-	</script>
-   <?php
-   }
-   else { ?>
-	<script language = "javascript" type = "text/javascript">
-	   alert('Message failed.  Please, send an email to ' .$mailTo);
-	   window.location = 'contactForm.html';
-	</script>
-   <?php
-   }
+   mail("lingenedil@gmail.com", $subject, $body, $head);
+    print "<script language = \"javascript\" type = \"text/javascript\">".
+	   "alert('Thank you for contacting us! We will reply to you shortly.');".
+	   "window.location = 'contact_us.html';".
+	"</script>";
+    die();
+}
 ?>
